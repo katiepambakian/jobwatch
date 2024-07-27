98dfa63100d8f66181549487a6d39e45cabaa479
Isabella <80723693+Isabella-Mclean@users.noreply.github.com>
2024-07-27 16:27:44 +0100

    Testing background image

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react';
 import Head from 'next/head';
 import { Box, Typography, Button } from '@mui/material';
 
+
 const fetchTitle = async (): Promise<string> => {
   // Simulating an API call or data fetching
   return 'Register Page'; // Return your desired title or data here
@@ -14,21 +15,20 @@ const RegisterPage: FC = async () => {
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>Register</title>
         <meta name="description" content="Login page description" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className="flex items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: 'url(/background.jpg)' }}>
+      </div>
       <Box 
       sx={{ 
         display: 'flex', 
         flexDirection: 'column', 
-        backgroundImage: 'url(../img/worker.jpg)',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
         alignItems: 'center', 
         mt: 8 }}>
-        <Typography variant="h1" component="h1">
+        <Typography variant="h3" component="p">
           Enter your email in the form below to be put on our waitlist!
         </Typography>
         
