cb5026f0fdd1581b362e1c38072b5e318cbfdaa9
Raj Aryan Upadhyaya <rajaryanupadhyaya@gmail.com>
2024-07-27 23:34:16 +0100

    Initial model for the db mover stuff, need to edit a bit more

--- scraper.py
+++ scraper.py
@@ -1,4 +1,7 @@
 import csv
+import psycopg2 
+import pandas as pd 
+from sqlalchemy import create_engine
 from jobspy import scrape_jobs
 
 country = "" # User input
@@ -18,4 +21,48 @@ jobs = scrape_jobs(
     )
 print(f"Found {len(jobs)} jobs")
 print(jobs.head())
-jobs.to_csv("jobs.csv", quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False) # to_excel
+jobs.to_csv("jobs.csv", quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False) # to csv
+
+
+# Integration to SQL PostGre:
+
+# establish connections 
+conn_string = 'postgres://postgres:pass@127.0.0.1/' # Whatever the sql link is. after the slash
+  
+db = create_engine(conn_string) 
+conn = db.connect() 
+conn1 = psycopg2.connect( 
+    database="", # insert name
+  user='postgres',  
+  password='pass',  
+  host='127.0.0.1',  
+  port= '5432'
+) 
+  
+conn1.autocommit = True
+cursor = conn1.cursor() 
+  
+# drop table if it already exists 
+cursor.execute('drop table if exists ') # insert name after space
+  
+sql = '''CREATE TABLE airlines_final(id int , 
+day int ,airline char(20),destination char(20));''' # replace with whatever necessary
+  
+cursor.execute(sql) 
+  
+# import the csv file to create a dataframe 
+df = pd.read_csv("jobs.csv")
+# Create DataFrame 
+print(df) 
+  
+# converting data to sql 
+df.to_sql('airlines_final', conn, if_exists= 'replace') 
+  
+# fetching all rows 
+sql1='''select * from airlines_final;''' # fix this
+cursor.execute(sql1) 
+for i in cursor.fetchall(): 
+    print(i) 
+  
+conn1.commit() 
+conn1.close()
