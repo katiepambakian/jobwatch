5496979ca2748503c4dcf0d86dfbeee7e05ce2ec
Raj Aryan Upadhyaya <upadhyra@tcd.ie>
2024-07-27 16:45:57 +0100

    Found smth for web scraping, will need:
    1) a way to link the jobs dataset to a database
    
    2) user input for the country, location, and specific search term/job title

---
+++ scraper.py
@@ -0,0 +1,20 @@
+import csv
+from jobspy import scrape_jobs
+
+country = "" # User input
+Locator = "" # User input
+
+jobs = scrape_jobs(
+    site_name=["indeed", "linkedin", "zip_recruiter", "glassdoor"],
+    search_term="software engineer",
+    location = Locator,
+    results_wanted=20,
+    hours_old=72, # (only Linkedin/Indeed is hour specific, others round up to days old)
+    country_indeed=country,  # only needed for indeed / glassdoor
+    
+    # linkedin_fetch_description=True # get full description , direct job url , company industry and job level (seniority level) for linkedin (slower)
+    # proxies=["208.195.175.46:65095", "208.195.175.45:65095", "localhost"],
+    )
+print(f"Found {len(jobs)} jobs")
+print(jobs.head())
+jobs.to_csv("jobs.csv", quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False) # to_excel
