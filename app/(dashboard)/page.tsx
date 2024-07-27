bdbf237c84245e45338e4595d3e276fae6c3be46
Katie Pambakian <katie.pambakian@icloud.com>
2024-07-27 14:54:03 +0100

    test commit

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -21,11 +21,9 @@ export default async function ProductsPage({
       <div className="flex items-center">
         <TabsList>
           <TabsTrigger value="all">All</TabsTrigger>
-          <TabsTrigger value="active">Active</TabsTrigger>
-          <TabsTrigger value="draft">Draft</TabsTrigger>
-          <TabsTrigger value="archived" className="hidden sm:flex">
-            Archived
-          </TabsTrigger>
+          <TabsTrigger value="Applied">applied</TabsTrigger>
+          <TabsTrigger value="Successful">Successful</TabsTrigger>
+          <TabsTrigger value="Unsuccessful">Unsuccessful</TabsTrigger>
         </TabsList>
         <div className="ml-auto flex items-center gap-2">
           <Button size="sm" variant="outline" className="h-8 gap-1">
