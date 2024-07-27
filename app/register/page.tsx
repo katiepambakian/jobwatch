e4da3281fd58489892474d7fa101bbbafc338ca3
Isabella <80723693+Isabella-Mclean@users.noreply.github.com>
2024-07-27 16:30:03 +0100

    Edited image, removed problem files

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -19,6 +19,9 @@ const RegisterPage: FC = async () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className="flex items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: 'url(/background.jpg)' }}>
+      </div>
+
       <Box 
       sx={{ 
         display: 'flex', 
